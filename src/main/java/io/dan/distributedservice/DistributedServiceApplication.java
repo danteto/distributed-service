f6b588c194cbeedc14fe6dd71ed7f4c0b98aab5f
dan <danbeman@yandex.com>
2021-03-31 20:54:19 +0200

    init

---
+++ src/main/java/io/dan/distributedservice/DistributedServiceApplication.java
@@ -0,0 +1,13 @@
+package io.dan.distributedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dan/distributedservice/DistributedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dan.distributedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
